45663ecc89d5edbc974e5681a90766dad6b0560f
Simon Schwarz <simon.schwarz884@gmail.com>
2023-04-08 12:25:00 +0200

    Add telescope

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -5,7 +5,20 @@ plugins = {
 	lazy = false,
 	cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
     	build = ":TSUpdate",
-    }
+    },
+    {
+        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        lazy = false,
+	dependencies = { 'nvim-lua/plenary.nvim' },
+	keys = {
+	    {
+		"<leader>ff",
+    	    	function()
+		  require("telescope.builtin").find_files()
+		end,
+    	    }
+	}
+    },
 }
 require("plugins.lazy")
 require("lazy").setup(plugins)
