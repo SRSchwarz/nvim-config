72be40f381d0b98d592db77f66cbf752e8dc0251
Simon Schwarz <simon.schwarz884@gmail.com>
2023-04-08 12:00:48 +0200

    Add tree sitter

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,3 +1,11 @@
-plugins = {}
+plugins = {
+    "nvim-lua/plenary.nvim",
+    {
+        "nvim-treesitter/nvim-treesitter",
+	lazy = false,
+	cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
+    	build = ":TSUpdate",
+    }
+}
 require("plugins.lazy")
 require("lazy").setup(plugins)
